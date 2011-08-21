3f4a44b1598aab87e4c8844dce882d30c41133c6
Jono <jono.oshea@gmail.com>
2011-08-21 12:06:09 -0700

    asdsda

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,14 +1,16 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-
+  
   def facebook
+    # You need to implement the method below in your model
     @user = User.find_for_facebook_oauth(env["omniauth.auth"], current_user)
+
     if @user.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
       sign_in_and_redirect '/', :event => :authentication
     else
       session["devise.facebook_data"] = env["omniauth.auth"]
-      redirect_to '/s'
+      redirect_to '/'
     end
-  end  
+  end
   
 end
