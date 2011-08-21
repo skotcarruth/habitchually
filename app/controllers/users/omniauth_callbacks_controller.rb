9cb86eae71618dd81f061add91564ae4394b33ea
Jono <jono.oshea@gmail.com>
2011-08-21 11:59:26 -0700

    asds

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,7 +7,7 @@ class User::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect '/', :event => :authentication
     else
       session["devise.facebook_data"] = env["omniauth.auth"]
-      redirect_to '/'
+      redirect_to '/s'
     end
   end  
   
